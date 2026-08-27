5d417388dcfc5057c43d136d2c8bb33ec1d61794
hajimammada <hajimammada@gmail.com>
2026-08-27 20:03:41 +0400

    release: v3.3.3 - enable _worker.js edge routing for Cloudflare Pages full-stack API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,7 +36,7 @@ import {
   RelayManager 
 } from './utils/api';
 
-const APP_VERSION = 'v3.3.2';
+const APP_VERSION = 'v3.3.3';
 
 export default function App() {
   const [settings, setSettings] = useState(() => getStoredSettings());
